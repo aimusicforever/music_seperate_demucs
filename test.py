550eb817eb52438f756834f1d70d59ae63a31ad4
“ws” <simonws@163.com>
2024-08-17 22:53:48 +0800

    add test

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 # app.py
 from datetime import datetime
 from flask import Flask
+import requests
 
-# app = Flask(__name__="__main__")
+
+def launch():
+    local_url = "http://127.0.0.1:40000/seperate"
+    file = {'file': open("./ad_jianchidaodi.mp3", 'rb')}
+
+    res = requests.post(url=local_url,
+                        files=file,
+                        data={"filename": "ad_jianchidaodi.mp3"},
+                        timeout=100000)
+
+
+if __name__ == '__main__':
+    launch()
+    
 
 # @app.route("/")
 # def index():
@@ -12,7 +26,4 @@ from flask import Flask
 
 
 
-def get_current_time():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-print("time==", get_current_time())
